01c7c317f3ddbefe5fe4c057b474657b21d81990
futureman2007 <48860149+futureman2007@users.noreply.github.com>
2019-06-25 01:10:37 +0200

    first commit

---
+++ DecoratorPattern/Start_Oxygen_EE.bat
@@ -0,0 +1,5 @@
+@echo off
+SET JAVA_HOME=C:\app\Java\jdk-9.0.1
+SET PATH=%JAVA_HOME%\bin;%PATH%
+SET ECLIPSE_HOME=C:\Developing\Eclipse\Oxygen_2\eclipse
+@start %ECLIPSE_HOME%\eclipse.exe -data C:\Users\Stephan user\Desktop\FH\Semester_6\SAT\Code\Pattern\DecoratorPattern -vm %JAVA_HOME%\bin\javaw -vmargs -Dfile.encoding=UTF-8 -Xms128m -Xmx3072m -XX:MaxHeapSize=768m -XX:MaxPermSize=512m
